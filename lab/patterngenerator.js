440987405c741ed58c3f7a1e96e96b886a278c2c
Kamila Ortiz <ortik455@newschool.edu>
2015-04-02 05:33:28 +0000

    should work js

--- lab/patterngenerator.js
+++ lab/patterngenerator.js
@@ -1,25 +1,12 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Javascript Pattern</title>
-	</head>
+console.log("smiley face pattern");
 
-	<body>
-	    <p>
-	        console.log("smiley face pattern");
+for (var multiplier = 1; multiplier < 10; multiplier++) {
+	console.log (multiplier*10);
 
-            for (var multiplier = 1; multiplier < 10; multiplier++){
-            	console.log (multiplier*10);
-            
-            	var smiley = "☻";
-            	
-            	for (var a = 0; a < 5; a++) {
-            			smiley = smiley + "☻☺☻";
-            			console.log(smiley);
-            	}
-            }
-	    </p>
-
-	</body>
-
-</html>
+	var smiley = "☻";
+	
+	for (var a = 0; a < 5; a++) {
+			smiley = smiley + "☻☺☻";
+			console.log(smiley);
+	}
+}
