8523e7df6022ad18e5d3c114020522ecc17513bb
Kamila Ortiz <ortik455@newschool.edu>
2015-04-09 15:26:13 +0000

    fixed yscroll

---
+++ studio/csstypeface/typeface.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    $(".element").hover(function() {
+    $(".mask").fadeIn(2000);
+});
+
+$(".letter-n").click(function() {
+    
+    $(".n-info").show();
+})
+});
