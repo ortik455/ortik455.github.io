08df5840c8f889904d20f79030227d4fcb824ab8
Kamila Ortiz <ortik455@newschool.edu>
2015-05-01 15:37:52 +0000

    jquery

--- studio/dice/game.js
+++ studio/dice/game.js
@@ -1,19 +1,31 @@
-var photos = 
-["http://graphics8.nytimes.com/images/2008/07/01/science/0709-lede-IRAN.jpg", 
-"http://www.coverbrowser.com/image/national-geographic/1034-3.jpg",
-"http://img.timeinc.net/time/magazine/archive/covers/1994/1101940627_400.jpg",
-"http://www.sree.net/teaching/lateditors_files/7235256.jpg",
-"http://images.china.cn/images1/200802/421701.jpg",
-"https://ethicsinediting.files.wordpress.com/2009/04/charlotteobserver1.jpg",
-"http://upload.wikimedia.org/wikipedia/en/b/b7/Beirut-smoke.jpg",
-"http://www.ronruelle.com/images/ccaart131week5/ccadetrich2.jpg",
-"http://morris1991.bhstudents.net/wp-content/uploads/2014/04/pope2.jpg",
-"https://damnuglyphotography.files.wordpress.com/2009/04/effed_with.jpg?w=640"];
+$( ".s:nth-child(even)" ).draggable({
+  axis: "y",
+  containment: '.tile-wrapper',
+  start: function() {
+    $('.s').removeClass('current');
+    $(this).css({ 'z-index' : 500 });
+    $(this).addClass('current');
+  },
+  stop: function() {
+    // $(this).css('z-index', 0);
+  }
+});
 
-var selectedphoto = false;
+$( ".s:nth-child(odd)" ).draggable({
+  axis: "x",
+  containment: '.tile-wrapper',
+  start: function() {
+    $('.s').removeClass('current');
+    $(this).css({ 'z-index' : 500 });
+    $(this).addClass('current');
+  },
+  stop: function() {
+    // $(this).css('z-index', 0);
+  }
+});
 
-$(".select-button").click(function() {
-
-$(".image-wrapper").html('<img src="' + photos[Math.floor(Math.random()*photos.length)]+ '">');
-  
+$('.t-button').click(function() {
+    var $tile = $('.s.current');
+    
+    $tile.css({ 'opacity' : .5 });
 });
