405d2e83cb5f5b98ec565727e44220d2062dc205
Kamila Ortiz <ortik455@newschool.edu>
2015-05-05 14:07:13 +0000

    transparency

--- studio/dice/game.js
+++ studio/dice/game.js
@@ -1,3 +1,13 @@
+var pages = ["ball.html", "haiti.html", "missile.html", "natgeo.html", "oj.html",
+"pope.html", "smoke.html", "soldier.html", "sun.html", "train.html"]; 
+ 
+
+$('.select-button').click(function() {
+    var random_num = Math.floor(Math.random() * pages.length);
+    window.location = pages[random_num];
+});
+
+
 $( ".s:nth-child(even)" ).draggable({
   axis: "y",
   containment: '.tile-wrapper',
@@ -24,8 +34,34 @@ $( ".s:nth-child(odd)" ).draggable({
   }
 });
 
-$('.t-button').click(function() {
+/*$('.t-button').click(function() {
     var $tile = $('.s.current');
-    
     $tile.css({ 'opacity' : .5 });
+});*/
+    
+$('.t-button').click(function(){
+    var $tile = $('.s.current');
+
+    if ($tile.css('opacity') == 1) {
+        $tile.css('opacity','0.8');
+    } else if ($tile.css('opacity') >= 0.80) {
+        $tile.css('opacity','0.6');
+    } else if ($tile.css('opacity') >= 0.60) {
+        $tile.css('opacity','0.4');
+    } else if ($tile.css('opacity') >= 0.40) {
+        $tile.css('opacity','0.2');
+    } else {
+        $tile.css('opacity','1');
+    }
+}); 
+
+
+$('.f-button').click(function() {
+    var $tile = $('.s.current');
+    $tile.css({ "-webkit-transform" : "rotateY(+180deg)" });
+});
+
+$('.r-button').click(function() {
+    var $tile = $('.s.current');
+    $tile.css({ "-webkit-transform" : "rotate(+90deg)" });
 });
